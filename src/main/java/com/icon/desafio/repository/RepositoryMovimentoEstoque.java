8dc6285675de077bd48ab0be862f5488461eec95
AndreiSchalinski <andreischalinski@hotmail.com>
2025-06-02 10:19:00 -0300

    Implementação de query para gerar relatório

--- src/main/java/com/icon/desafio/repository/RepositoryMovimentoEstoque.java
+++ src/main/java/com/icon/desafio/repository/RepositoryMovimentoEstoque.java
@@ -9,9 +9,17 @@ import com.icon.desafio.model.MovimentoEstoqueModel;
 
 public interface RepositoryMovimentoEstoque extends JpaRepository<MovimentoEstoqueModel, Long> {
 
-    @Query("SELECT p.descricao AS descricao, SUM(m.quantidadeMovimentada) AS totalQuantidade\r\n" + //
-            "    FROM MovimentoEstoqueModel m JOIN m.produto p\r\n" + //
-            "    GROUP BY p.descricao")
+    @Query("""
+            SELECT p.descricao AS descricao,
+                   SUM(m.quantidadeMovimentada) AS totalQuantidadeMovimentada,
+                   SUM(m.quantidadeMovimentada * m.valorVenda) AS totalLucroBruto,
+                   SUM(m.quantidadeMovimentada * m.valorVenda) - SUM(p.precoFornecedor) AS totalLucroLiquido,
+                   SUM(p.precoFornecedor) AS totalFornecedor
+            FROM MovimentoEstoqueModel m
+            JOIN m.produto p
+            WHERE m.tipoMovimentacao = com.icon.desafio.enums.TipoMovimentacao.SAIDA
+            GROUP BY p.id
+            """)
     List<Object[]> gerarRelatorioLucros();
 
 }
